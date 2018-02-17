12655734fb396efa72cac22ba3fbf1b74c502aff
LexNavarro <sharkvarro@gmail.com>
2018-02-17 14:09:51 -0500

    Added counter file

---
+++ counter.py
@@ -0,0 +1,33 @@
+for i in range(10):
+    print('The number {}'.format(i))
+
+names = ['Alexander', 'David', 'Navarro']
+
+for name in names:
+    print('The name is: {}'.format(name))
+
+numbers = [5, 9, 25]
+
+for number in numbers:
+    print('The number is: {}'.format(number))
+
+A = 56
+B = 'New text item.'
+C = ['this', 'is', 'a', 'list', 'of', 'strings']
+
+values = [A, B, C]
+
+for value in values:
+    print('The value is: {}'.format(value))
+
+A = 100
+B = 200
+C = 300
+
+values = [A, B, C]
+
+for value in values:
+    print('The values are: {}'.format(value))
+
+
+print('done')
